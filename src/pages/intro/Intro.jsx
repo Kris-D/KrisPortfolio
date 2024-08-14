d110c27187951fdf9786434710c490e68b6b8171
Christopher Isioma Dunkwu <chriscjd001@gmail.com>
2024-08-14 02:54:26 +0100

    intro animation

--- src/pages/intro/Intro.jsx
+++ src/pages/intro/Intro.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./Intro.module.css";
+import logo from '../../assets/image/kLogok.png';
 const Intro = () => {
   return (
-    <div className="intro">
-      <h1>Welcome to KrisPortfolio</h1>
-      <p>Loading...</p>
+    <div className={styles.container}>
+      <div className={styles.intro}>
+        <svg viewBox="0 0 740 160">
+          <text
+            x="23%"
+            y="23%"
+            dy="4rem"
+            textAnchor="middle"
+            className={styles.textbody}
+          >
+            Kris-D
+          </text>
+          <text
+            x="23%"
+            y="23%"
+            dy="4rem"
+            dx="3.2em"
+            textAnchor="middle"
+            className={styles.dot}
+          >
+            portfolio
+          </text>
+        </svg>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
